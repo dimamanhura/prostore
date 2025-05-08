30675d7966338ccbe2104ecfd900eb127cd95f18
Manhura Dima <manguradima@gmail.com>
2025-05-08 18:45:25 +0300

    Add carousel with featured products to the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,22 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.action";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.action";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
-      <ProductList data={latestProducts} title="Newest Arrivals" />
+      {featuredProducts?.length > 0 && (
+        <ProductCarousel
+          featuredProducts={featuredProducts}
+        />
+      )}
+      <ProductList
+        data={latestProducts}
+        title="Newest Arrivals"
+      />
     </>
   );
 };
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -157,3 +157,17 @@ export async function getAllCategories() {
 
   return data;
 };
+
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: {
+      isFeatured: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 4,
+  });
+
+  return convertToPlainObject(data);
+};
